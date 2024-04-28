629558a8366e2ec2f3684ed23353bd31afabce90
jespertill <jesper.t.e@web.de>
2024-04-28 13:38:30 +0200

    interaction.js Kommentare
    Aufräumen und Dokumentieren/Kommentieren

--- src/interaction.js
+++ src/interaction.js
@@ -1,25 +1,38 @@
 import TWEEN from '@tweenjs/tween.js';
 import * as THREE from 'three';
-import { createTextMesh, heavyRotGroup, inhaltGroup, minCameraZ, maxCameraZ } from './script.js';
+import { createTextMesh, heavyRotGroup, inhaltGroup, minCameraZ, maxCameraZ } from './script.js'; //Import der benötigten Variablen aus script.js
 
+// TextMeshes werden in einer Map gespeichert, um sie später wieder entfernen zu können
 const textMeshMap = new Map();
 
-// Funktion zum Aktualisieren der Raycaster-Interaktion von Objekten + export für Verwendung in anderen scripten
+/**
+ * Aktualisiert die Raycaster-Interaktion basierend auf der Mausposition und der Kamera.
+ * 
+ * @param {THREE.Raycaster} raycaster - Der Raycaster, der für die Interaktion verwendet wird.
+ * @param {THREE.Vector2} mouse - Die aktuelle Mausposition.
+ * @param {THREE.Camera} camera - Die Kamera, die für die Interaktion verwendet wird.
+ * @param {THREE.Group} objectsGroup - Die Gruppe von Objekten, auf die der Raycaster angewendet wird.
+ * @param {THREE.Object3D} lastIntersected - Das zuletzt intersected Objekt.
+ * @returns {THREE.Object3D} - Das aktuell intersected Objekt.
+ */
 export function updateRaycasterInteraction(raycaster, mouse, camera, objectsGroup, lastIntersected) {
     // Überprüfung, ob die Kamera innerhalb des festgelegten Z-Bereichs liegt, in script.js definiert
     if (camera.position.z < minCameraZ || camera.position.z > maxCameraZ) {
         return lastIntersected;
     }
-
+    
     raycaster.setFromCamera(mouse, camera);
     let intersects = raycaster.intersectObjects(objectsGroup.children);
 
+    // Wenn ein Objekt gehovert wird, wird es animiert
     if (intersects.length > 0) {
         const intersected = intersects[0].object;
+        // Wenn das gehoverte Objekt ein neues Objekt ist, wird das alte zurückgesetzt
         if (lastIntersected !== intersected) {
             if (lastIntersected) {
                 resetObject(lastIntersected);
             }
+            // Wenn das gehoverte Objekt noch nicht gehovert wurde, wird es animiert
             if (!intersected.userData.isHovered && !intersected.userData.animationActive) {
                 moveObject(intersected, intersected.position.z + 50, 200);
             }
@@ -33,6 +46,14 @@ export function updateRaycasterInteraction(raycaster, mouse, camera, objectsGrou
 }
 
 
+/**
+ * Bewegt ein Objekt zu einer bestimmten Z-Koordinate über eine angegebene Dauer.
+ * @param {Object3D} obj - Das zu bewegende Objekt.
+ * @param {number} targetZ - Die Ziel-Z-Koordinate, zu der das Objekt bewegt werden soll.
+ * @param {number} duration - Die Dauer der Animation in Millisekunden.
+ * @returns {Promise<void>} Ein Promise, das erfüllt wird, wenn die Animation abgeschlossen ist.
+ * @throws {Error} Wenn ein Fehler beim Animieren des Objekts auftritt.
+ */
 async function moveObject(obj, targetZ, duration) {
     try {
         if (!obj.userData.isHovered) {
@@ -57,6 +78,12 @@ async function moveObject(obj, targetZ, duration) {
     }
 }
 
+/**
+ * Zeigt den Namen des Songs an.
+ * 
+ * @param {Object} obj - Das Objekt, das den Song repräsentiert.
+ * @returns {Promise<THREE.Mesh>} - Das TextMesh-Objekt, das den Songnamen darstellt.
+ */
 async function displaySongName(obj) {
     let songNameTextMesh = await createTextMesh(obj.userData.name, 10, heavyRotGroup.position.x, heavyRotGroup.position.y, heavyRotGroup.position.z, 0);
     if (!textMeshMap.has(obj)) {
@@ -66,6 +93,12 @@ async function displaySongName(obj) {
     return songNameTextMesh;
 }
 
+/**
+ * Zeigt den Namen des Künstlers eines Songs an.
+ * 
+ * @param {Object} obj - Das Objekt, das die Informationen über den Song enthält.
+ * @returns {Promise<THREE.Mesh>} - Ein Promise, das ein TextMesh-Objekt zurückgibt, das den Namen des Künstlers darstellt.
+ */
 async function displaySongArtist(obj) {
     const artistsArray = obj.userData.artists;
     const artistsNames = artistsArray.map(artist => artist.name).join(", ");
@@ -77,8 +110,14 @@ async function displaySongArtist(obj) {
     return songArtistTextMesh;
 }
 
-
-
+/**
+ * Zentriert den TextMesh an der angegebenen Position.
+ * 
+ * @param {THREE.Mesh} textMesh - Das TextMesh, das zentriert werden soll.
+ * @param {number} x - Die x-Koordinate der gewünschten Position.
+ * @param {number} y - Die y-Koordinate der gewünschten Position.
+ * @param {number} z - Die z-Koordinate der gewünschten Position.
+ */
 function centerTextMesh(textMesh, x, y, z) {
     // Stelle sicher, dass die Bounding Box berechnet ist
     textMesh.geometry.computeBoundingBox();
@@ -92,10 +131,13 @@ function centerTextMesh(textMesh, x, y, z) {
     textMesh.position.y = y - size.y / 2;
     textMesh.position.z = z;
   }
-  
-  
-
-// Funktion zum Zurücksetzen von Objekten
+    
+/**
+ * Setzt das übergebene Objekt zurück, indem es die entsprechenden Eigenschaften zurücksetzt
+ * und die TextMeshes aus der Szene entfernt.
+ * 
+ * @param {Object3D} obj - Das Objekt, das zurückgesetzt werden soll.
+ */
 function resetObject(obj) {
     if (obj.userData.isHovered) {
         obj.userData.isHovered = false;
