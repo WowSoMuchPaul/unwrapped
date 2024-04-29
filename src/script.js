9be7098e1c5dc57d5c6552f7eeecc7bc19588824
JDuenninger <102795149+JDuenninger@users.noreply.github.com>
2024-04-29 21:11:34 +0200

    Text Parameter, Playlist Anpassungen und Icons

--- src/script.js
+++ src/script.js
@@ -14,6 +14,7 @@ let sizes, canvas, scene, camera, helper, renderer, controls, trackControls, hem
 var inEinemBereich = false;
 var tweenAktiviert = false;
 var freeMovement = true;
+//Navigation Konstanten
 const targetPoints = {};
 const bereichOffsetVorne = 400;
 const bereichDampingVorne = bereichOffsetVorne / 2;
@@ -23,6 +24,11 @@ const zoomSpeedNorm = 0.3;
 const zoomSpeedBereich = 0.02;
 const tweenStartDistance = 10;
 const cameraTargetDistance = 100;
+// Textgrößen Konstanten
+const headlineSize = 40;
+const textSize = 10;
+const textSmallSize = 5;
+const textBigSize = 20;
 
 const stats = new Stats();
 // stats.showPanel(0);
@@ -36,6 +42,7 @@ window.addEventListener('mousemove', (event) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }, false);
+
 
 /**cursor */
 const cursor = {};
@@ -65,10 +72,12 @@ function init() {
     /**
      * Camera
      */
-    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    camera = new THREE.PerspectiveCamera(92, sizes.width / sizes.height)
+    //kamera setup mit far und focus für bessere Darstellung
+    // camera = new THREE.PerspectiveCamera(92, sizes.width / sizes.height, 0.2,520)
     camera.position.z = 3000;
-    camera.far = 10;
-    camera.focus = 1000;
+    // camera.far = 10;
+    // camera.focus = 1000;
     scene.add(camera);
     lastCamPosition = camera.position.z;
     
@@ -347,7 +356,7 @@ function handleIntersectedObject(intersectedObject) {
         intersectedObject.isHovered = true;
         // Erstellen von Künstler:innen Name neben den Bildern 
         // console.log(intersectedObject.userData.name);
-        // const rotText = createTextMesh( intersectedObject.userData.name, 10, intersectedObject.position.x, intersectedObject.position.y -50, targetPoints.onRepeat , 0);
+        // const rotText = createTextMesh( intersectedObject.userData.name, textSize , intersectedObject.position.x, intersectedObject.position.y -50, targetPoints.onRepeat ,0,0,0x000000, 1,'W95FA_Regular.typeface');
         // console.log(rotText);
     }
     // aktualisieren des letzten getroffenen Objekts
@@ -373,6 +382,7 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime();
     const deltaTime = elapsedTime - previousTime;
     previousTime = elapsedTime;
+    lastCamPosition = Math.round(camera.position.z);
 
     //Animate camera
     const parallaxX = cursor.x * 50;
@@ -384,15 +394,29 @@ const tick = () => {
         checkCamPosition();
         //console.log("Es bewegt sich. " + Math.round(camera.position.z));
     }
-    lastCamPosition = Math.round(camera.position.z);
-     //console.log("Checking position:", lastCamPosition, "vs", targetPoints.onRepeat);
-    //  if(lastCamPosition <= targetPoints.)
-    if(lastCamPosition < targetPoints.onRepeat){
-        createPlaylistButton();
-    }
-     
-
     
+    // Prüfen, ob die Kamera im Bereich der Playlist ist
+    if(lastCamPosition <= targetPoints.onRepeat){
+        if(document.getElementById("createPlaylist-btn") == null){
+            createPlaylistButton();
+        }
+    }
+
+    // Prüfen, ob die Kamera im Bereich der Playlist ist
+    if (lastCamPosition <= targetPoints.playlist){    
+        if(document.getElementById("createPlaylist-btn") != null){
+            document.getElementById("createPlaylist-btn").style.display = "none";
+        }
+    }
+
+    // Prüfen, ob die Kamera im Bereich der Playlist ist
+    if (lastCamPosition >= targetPoints.onRepeat){    
+        if(document.getElementById("createPlaylist-btn") != null){
+            document.getElementById("createPlaylist-btn").style.display = "none";
+        }
+    }
+    
+    iconAnimationPl()
 
     //console.log(lastCamPosition);
    
@@ -425,10 +449,49 @@ const tick = () => {
 }
 
 
-function createTextMesh(text, fontsize, x, y, z) {
+// function createTextMesh(text, fontsize, x, y, z, rotationX, rotationY, color, opacity, fontName) {
+//     const fontLoader = new FontLoader()
+//     fontLoader.load(
+//         `../fonts/${fontName}.json`,
+//         (font) => {
+//             const textGeometry = new TextGeometry(
+//                 text, {
+//                     font: font,
+//                     size: fontsize,
+//                     height: 1,
+//                     curveSegments: 12,
+//                     bevelEnabled: true,
+//                     bevelThickness: 0.03,
+//                     bevelSize: 0.02,
+//                     bevelOffset: 0,
+//                     bevelSegments: 5
+//                 }
+//                 )
+//                 const textMaterial = new THREE.MeshBasicMaterial();
+//                 let textMesh = new THREE.Mesh(textGeometry, textMaterial);
+//                 //textMesh.receiveShadow = true;
+//                 inhaltGroup.add(textMesh);
+//                 textMesh.position.x = x;
+//                 textMesh.position.y = y;
+//                 textMesh.position.z = z;
+//                 textMesh.rotateY(rotationY * (Math.PI / 180))
+//                 textMesh.rotateX(rotationX * (Math.PI / 180));
+//                 textMaterial.color = color || new THREE.Color(0xffffff);
+//                 textMaterial.opacity = opacity || 1;
+
+//                 //resolve(textMesh); // Lösen Sie die Promise mit textMesh
+//             },
+//             // undefined, // onProgress callback not needed
+//             // (error) => reject(error) // Reject the Promise on error
+//         )
+//     };
+
+    // Funktion zum erstellen von TextMeshes
+export async function createTextMesh(text, fontsize, x, y, z,  rotationX, rotationY, color, opacity, fontName) {
+    return new Promise((resolve, reject) => {
     const fontLoader = new FontLoader()
     fontLoader.load(
-        '../fonts/W95FA_Regular.typeface.json',
+        `../fonts/${fontName}.json`,
         (font) => {
             const textGeometry = new TextGeometry(
                 text, {
@@ -442,59 +505,36 @@ function createTextMesh(text, fontsize, x, y, z) {
                     bevelOffset: 0,
                     bevelSegments: 5
                 }
-                )
-                const textMaterial = new THREE.MeshBasicMaterial();
-                let textMesh = new THREE.Mesh(textGeometry, textMaterial);
-                //textMesh.receiveShadow = true;
-                inhaltGroup.add(textMesh);
-                textMesh.position.x = x;
-                textMesh.position.y = y;
-                textMesh.position.z = z;
-                //textMesh.rotateY(rotationY * (Math.PI / 180))
-                //resolve(textMesh); // Lösen Sie die Promise mit textMesh
-            },
-            // undefined, // onProgress callback not needed
-            // (error) => reject(error) // Reject the Promise on error
-        )
-    };
-    stats.end();
-
-    function createIconMesh(text, fontsize, x, y, z, rotationX, rotationY, color, opacity) {
-        const fontLoader = new FontLoader()
-        fontLoader.load(
-            '../fonts/Yarndings 12_Regular.json',
-            (font) => {
-                const iconGeometry = new TextGeometry(
-                    text, {
-                        font: font,
-                        size: fontsize,
-                        height: 0.2,
-                        curveSegments: 12,
-                        bevelEnabled: true,
-                        bevelThickness: 0.03,
-                        bevelSize: 0.02,
-                        bevelOffset: 0,
-                        bevelSegments: 5
-                    }
-                    )
-                    const iconMaterial = new THREE.MeshBasicMaterial();
-                    let iconMesh = new THREE.Mesh(iconGeometry, iconMaterial);
-                    //iconMesh.receiveShadow = true;
-                    inhaltGroup.add(iconMesh);
-                    iconMesh.position.x = x;
-                    iconMesh.position.y = y;
-                    iconMesh.position.z = z;
-                    iconMaterial.color = color;
-                    iconMaterial.opacity = opacity;
-                    
-                    iconMesh.rotateX(rotationY * (Math.PI / 180));
-                    iconMesh.rotateY(rotationY * (Math.PI / 180));
-                    //resolve(iconMesh); // Lösen Sie die Promise mit textMesh
-                },
-                // undefined, // onProgress callback not needed
-                // (error) => reject(error) // Reject the Promise on error
             )
-        };
+            const textMaterial = new THREE.MeshBasicMaterial();
+            let textMesh = new THREE.Mesh(textGeometry, textMaterial);
+            inhaltGroup.add(textMesh);
+            // Positionierung des TextMeshes
+            textMesh.position.x = x;
+            textMesh.position.y = y;
+            textMesh.position.z = z;
+            textMesh.rotateY(rotationY * (Math.PI / 180))
+            textMesh.rotateX(rotationX * (Math.PI / 180));
+            textMaterial.color = color || new THREE.Color(0xffffff);
+            textMaterial.opacity = opacity || 1;
+
+            // Fügt das TextMesh der inhaltGroup hinzu
+            inhaltGroup.add(textMesh);
+
+            // Auflösen des Promises mit dem erstellten TextMesh
+            textMesh.userData.name = text;
+            resolve(textMesh);
+        },
+        undefined,
+        (error) => {  // onError Handler
+            reject(error);
+        }
+    );
+});
+}
+
+stats.end();
+
 function createBildMesh(bildUrl, x, y, z, rotationY, bildGroesse) {
     //Bildtextur
     const texture = new THREE.TextureLoader().load(bildUrl);//'https://3.bp.blogspot.com/-Ol0cP_dxq7U/VWjIWBW2QpI/AAAAAAAAJxg/8ackwAwAYIE/s1600/JPx7R.jpg' );
@@ -577,7 +617,7 @@ function createQuaderMesh(x,y,z,rotationX, rotationY, size, color){
    frameMesh.position.z = z;
    frameMesh.rotateY(rotationY * (Math.PI/180));
    frameMesh.rotateX(rotationX * (Math.PI/180));
-    frameMesh.material.color = color;
+    material.color = color;
 
    return frameMesh;
 }
@@ -587,12 +627,12 @@ function createProfil() {
     let recentlyPlayed = getRecentlyPlayed();
     let winkel = 0;
     createBildMesh(profil.imageUrl, + 80, 0, targetPoints.profil, winkel, 50);
-    createTextMesh("Hey \n" + profil.name + " !", 10, -80, 30, targetPoints.profil, 0);
-    createTextMesh("Followers: " + profil.follower.toString(), 3, 55, -30, targetPoints.profil, winkel);
+    createTextMesh("Hey \n" + profil.name + " !", textSize, -80, 30, targetPoints.profil,0,0,0x000000, 1,'W95FA_Regular.typeface');
+    createTextMesh("Followers: " + profil.follower.toString(), textSmallSize, 55, -30, targetPoints.profil,winkel,0,0x000000, 1,'W95FA_Regular.typeface');
 
     let recGroupX = -80;
     let recGroupY = -17;
-    let recText = 2;
+    let recText = textSize;
     let recBildG = 25;
     let recBildRot = 0;
 
@@ -608,7 +648,7 @@ function createProfil() {
     createTextMesh(recentlyPlayed[2].name, recText, recGroupX + 1, recGroupY - 64, targetPoints.profil, recBildRot);
 
     createBildMesh(recentlyPlayed[3].image, recGroupX + 43, recGroupY - 48, targetPoints.profil, recBildRot, recBildG);
-    createTextMesh(recentlyPlayed[3].name, recText, recGroupX + 31, recGroupY - 64, targetPoints.profil, recBildRot);
+    createTextMesh(recentlyPlayed[3].name, recText, recGroupX + 31, recGroupY - 64, targetPoints.profil, recBildRot,0,0x000000, 1,'W95FA_Regular.typeface');
 }
 
 
@@ -618,15 +658,15 @@ function createTopArtist() {
     let i = 0;
     //console.log("createTopArtists, hier sind sie: " + topArtists);
     //console.log("Diese Profil gehört: " + profil.name);
-    createTextMesh(profil.name + "'s", 20, -300, 110, targetPoints.topArtist - 200, 0);
-    let headlineTwo = createTextMesh("\nTop Artists", 40, -300, 110, targetPoints.topArtist - 200, 0);
+    createTextMesh(profil.name + "'s", textBigSize, -300, 110, targetPoints.topArtist - 200,0,0,0x000000, 1,'W95FA_Regular.typeface');
+    let headlineTwo = createTextMesh("\nTop Artists", headlineSize, -300, 110, targetPoints.topArtist - 200,0, 0, 0x000000,0.7,'Jersey 15_Regular');
     while (i < topArtists.length) {
         let x = 100 + i * -15;
         let y = -60 + i * 25;
         let z = targetPoints.topArtist - (100 + i * 55);
         let BildMesh = createBildMesh(topArtists[i].imageUrl, x, y, z, 0, 65, 0);
         topArtists[i].mesh = BildMesh;//zwischenspeichern des Meshes im Array
-        let artistName = createTextMesh(topArtists[i].name, 5, x + 45, y + 17, z, 0);
+        let artistName = createTextMesh(topArtists[i].name, textSmallSize, x + 45, y + 17, z,0,0,0x000000, 1,'W95FA_Regular.typeface');
         i++;
     }
 }
@@ -663,7 +703,7 @@ function createHeavyRotation() {
         bildMesh.userData.name = heavyRotation[e].name;
         heavyRotGroup.add(bildMesh);
     }
-    createTextMesh("Your \nHeavy Rotation", 40, -200, 0, targetPoints.onRepeat - 300, 0);
+    createTextMesh("Your \nHeavy Rotation", headlineSize, -200, 0, targetPoints.onRepeat - 300,0, 0, 0x000000,0.7,'Jersey 15_Regular');
 }
 
 // Funktion zu Erstellen aller Hauptgruppen der Szene
@@ -676,42 +716,63 @@ function createTopSongs() {
         songs = JSON.parse(localStorage.getItem("topSongs"));
     }
     //console.log(songs);
-    createTextMesh("Deine Top 3 Songs", 5, -55, 15, targetPoints.topSong);
+    createTextMesh("Deine Top 3 Songs", headlineSize, -55, 15, targetPoints.topSong,0, 0, 0x000000,1,'Jersey 15_Regular');
     createBildMesh(songs[0].imageUrl, 0, 0, targetPoints.topSong, 0, 20);
-    createTextMesh("1: " + songs[0].name, 2, -10, -15, targetPoints.topSong);
+    createTextMesh("1: " + songs[0].name, textSmallSize, -10, -15, targetPoints.topSong,0,0,0x000000, 1,'W95FA_Regular.typeface');
     createBildMesh(songs[1].imageUrl, -30, -5, targetPoints.topSong, 0, 20);
-    createTextMesh("2: " + songs[1].name, 2, -40, -20, targetPoints.topSong);
+    createTextMesh("2: " + songs[1].name, textSmallSize, -40, -20, targetPoints.topSong,0,0,0x000000, 1,'W95FA_Regular.typeface');
     createBildMesh(songs[2].imageUrl, 30, -10, targetPoints.topSong, 0, 20);
-    createTextMesh("3: " + songs[2].name, 2, 20, -25, targetPoints.topSong);
+    createTextMesh("3: " + songs[2].name, textSmallSize, 20, -25, targetPoints.topSong,0,0,0x000000, 1,'W95FA_Regular.typeface');
+}
+
+async function iconAnimationPl(){
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+
+    // Modelle
+    const iconAl = null;
+    const iconRob = null;
+    
+    if(iconAl == null && iconRob == null){
+        console.log("i m a shit in this 1");
+    const iconAl = await createTextMesh("w", textBigSize-5, 55, 85, targetPoints.playlist,0, -25, 0x000000,0.3,'Yarndings 12_Regular');
+    const iconRob = await createTextMesh("x", textBigSize-10, -120, -75, targetPoints.playlist,0, 15, 0x000000,0.2,'Yarndings 12_Regular');
+        //animation der Modelle
+        console.log(iconAl);
+        console.log(iconRob);
+        // console.log(iconAl.rotation.y);
+        console.log(elapsedTime)
+        //  iconAl.rotation.y  += Math.sin(elapsedTime) * 0.9;
+        //iconAl.rotation.x += Math.cos(elapsedTime) * 0.5;
+        // iconAl.y = Math.sin(elapsedTime) * 5;
+        console.log(iconAl.x);
+        iconAl.x = Math.cos(elapsedTime) * -0.5;
+        // iconRob.rotationY = Math.sin(elapsedTime) * 2;
+        // iconRob.rotationX = Math.cos(elapsedTime) * 3;   
+        // iconRob.y = Math.sin(elapsedTime) * 5;
+        // iconRob.x = Math.cos(elapsedTime) * 5;
+    }
 }
 
 function createPlaylist(){
-    createTextMesh("Deine \nunwrapped \nPlaylist", 5, -55, 15, targetPoints.playlist);
-    
+    createTextMesh("Deine \nunwrapped \nPlaylist", headlineSize, -250, 85, targetPoints.playlist-40,0,25,0x000000, 1,'Jersey 15_Regular');
+    // createTextMesh("w", textBigSize-5, 55, 85, targetPoints.playlist,0, -25, 0x000000,0.3,'Yarndings 12_Regular');
+    // createTextMesh("x", textBigSize-10, -120, -75, targetPoints.playlist,0, 15, 0x000000,0.2,'Yarndings 12_Regular');
 }
 
 function createPlaylistButton(){
-    console.log('bin drinn')
     const button  = document.createElement('button');
     button.id = 'createPlaylist-btn';
     button.textContent = 'Create Playlist';
-    // button.style.textAlign = 'start';
     button.style.display = 'block';
     document.getElementById('playlistBttnContainer').appendChild(button);
     document.getElementById("createPlaylist-btn").addEventListener("click",setFestivalPlaylist);
 }
 
-// function playlistBtn(){
-//     if(lastCamPosition < 1000){
-//         console.log("bin drinn");
-//         console.log(button.name)
-// }
-// }
-
 function createEND(){
-    createTextMesh("THE END", 8, 0, 0, targetPoints.playlist-500);
-    createRingMesh(15,0,targetPoints.playlist-500,-55,25,0xffffff,150,160);
-    
+    createTextMesh("THE END", headlineSize, -40, 0, targetPoints.playlist-500,0, 0, 0x000000,0.7,'Jersey 15_Regular');
+    createRingMesh(15,0,targetPoints.playlist-500,-35,15,0x42887E,150,160);
     createQuaderMesh(0,0,targetPoints.playlist-500,20,10, 120, 0x8043E2);
 }
 
