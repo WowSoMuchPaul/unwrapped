d17a6344c25bffab3993d71819ff81a6012be27f
WowSoMuchPaul <paulvzmail@web.de>
2024-05-23 16:10:51 +0200

    Licht und andere Fixes

--- src/imports.js
+++ src/imports.js
@@ -1,4 +1,15 @@
+/**
+ * Modul, das alle benötigten Abhängigkeiten importiert.
+ * @module imports
+ */
 import * as THREE from 'three';
 import TWEEN from '@tweenjs/tween.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import playlistCover from '../static/images/playlistCover.jpg';
+import playlistCoverMid from '../static/images/playlistCoverMid.jpg';
+import playlistCoverShort from '../static/images/playlistCoverShort.jpg';
 
-export { THREE, TWEEN };
+export { THREE, TWEEN, FontLoader, TextGeometry, TrackballControls, GLTFLoader, playlistCover, playlistCoverMid, playlistCoverShort};
